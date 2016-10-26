81d9db07842b1b474771cfad6b0ab0f00a471870
santhosh <rm.santhosh1234@gmail.com>
2016-10-26 18:45:10 +0530

    udpdated index.html
    Added contents tp about me section

---
+++ js/skill.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('.adobe').css('width', '70%');
+  $('.html').css('width', '50%');
+  $('.css').css('width', '45%');
+  $('.lesssass').css('width', '50%');
+  $('.jquery').css('width', '70%');
+  $('.javascript').css('width', '70%');
+  $('.bootstrap').css('width', '90%');
+  $('.wordpress').css('width', '85%');
+  $('.ui').css('width', '70%');
+  $('.ux').css('width', '90%');
+  $('.rwd').css('width', '60%');
+  $('.mobile').css('width', '85%');
+  $('.rapidproto').css('width', '95%');
+});
